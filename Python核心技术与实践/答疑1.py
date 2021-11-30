52bc0e16beef92aab650a2925ea6ff7988157292
harverychina <harverychina@126.com>
2021-11-30 10:02:03 +0800

    为Python核心技术与实践 添加对象的比较、拷贝和答疑1内容

---
+++ Python核心技术与实践/答疑1.py
@@ -0,0 +1,17 @@
+# -*- coding:utf8 -*-
+# @Time：2021/11/30 9:12 上午
+# @Author： Huang Jeff
+# x = [1]
+#
+#
+# def func():
+#     x.append(2)
+#
+#
+# print(func())
+# print(x)
+
+t1 = (1, 2)
+t2 = (1, 2)
+print(t1 is t2)  # True
+# 交互方式是单独要编译变量，内存地址不相同，输出为False
