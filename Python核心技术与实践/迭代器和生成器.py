23fe79665115881d1e8cb2147012a99c78ed5731
harverychina <harverychina@126.com>
2021-12-08 10:56:32 +0800

    为Python核心技术与实践 metaclass和迭代器、生成器练习（加更新部分）

--- Python核心技术与实践/迭代器和生成器.py
+++ Python核心技术与实践/迭代器和生成器.py
@@ -2,3 +2,23 @@
 # @Time：2021/12/8 10:41 AM
 # @Author： Huang Jeff
 
+def is_iterable(param):
+    try:
+        iter(param)
+        return True
+    except TypeError:
+        return False
+
+
+params = [
+    1234,
+    '1234',
+    [1, 2, 3, 4],
+    set([1, 2, 3, 4]),
+    {1: 1, 2: 2, 3: 3, 4: 4},
+    (1, 2, 3, 4)
+]
+
+for param in params:
+    print('{} is iterable? {}'.format(param, is_iterable(param)))
+
